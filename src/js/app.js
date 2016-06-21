cbf426b0e44a1725b3584ad04e68eba5d14e5a11
Saivarun Pothuraju <pothuraju.saivarun@outlook.com>
2016-06-20 21:43:51 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
     var app = angular.module('app', []).config(function(){
         geocoder = new google.maps.Geocoder();
-    }).constant("APIKEY", "50032743e663e9f8b1de11ad23cca007");
+    }).constant("APIKEY", " "); //Add your own API Key for forecast.io here.
