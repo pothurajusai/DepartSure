4dc516ddfd3f7613991dc425043b16b14daa507d
Saivarun Pothuraju <pothuraju.saivarun@outlook.com>
2016-06-27 00:02:37 -0400

    Update mainCtrl.js

--- src/js/controllers/mainCtrl.js
+++ src/js/controllers/mainCtrl.js
@@ -1,16 +1,16 @@
     app.controller('MainController', function($scope, weatherIO){
 
-        $scope.showData = false;
+        $scope.showData = false; //Initially set to false. Controls the weather metrics display.
         $scope.searchAirport = function (){
 
               geocoder.geocode( { 'address': $scope.airportCode}, function(results, status)
               {
                 if (status == google.maps.GeocoderStatus.OK)
                 {
-                    $scope.airportName = results[0].address_components[0].short_name;
+                    $scope.airportName = results[0].address_components[0].short_name; //Sets the airport name (Or whatever location the maps api returns).
                         weatherIO.getWeather(results[0].geometry.viewport).then(function(data){
-                            setData(data);
-                            setGoogleMaps(results[0],data);
+                            setData(data); //Sets the data of all the weather metrics.
+                            setGoogleMaps(results[0],data); //Sets the google maps position.
                         }, function(err){
                             alert(err);
                         });
@@ -24,38 +24,40 @@
         }
 
         function SetupSkyIcon(weatherIcon){
-             var skycons = new Skycons({"color": "ORANGE"});
-             skycons.add("WeatherIcon", weatherIcon);
-             skycons.play();
+             var skycons = new Skycons({"color": "ORANGE"}); //Color of the Skycon can be changed here.
+             skycons.add("WeatherIcon", weatherIcon); // Initialize the weather icon.
+             skycons.play(); //Starts playing the animated weather icon. 
         }
 
 
         function setGoogleMaps(data, forecastData){
 
-            var Lat = data.geometry.viewport.f.b;
-            var Long = data.geometry.viewport.b.b;
+            var Lat = data.geometry.viewport.f.b; //Latitude property returned from the Google Maps API.
+            var Long = data.geometry.viewport.b.b; //Longitude property returned from the Google Maps API.
 
-            var myLatLng = new google.maps.LatLng(Lat,Long);
+            var LatLongObj = new google.maps.LatLng(Lat,Long); 
 
 
             var map = new google.maps.Map(document.getElementById("map"),
             {
             zoom: 13,
-            center: myLatLng,
+            center: LatLongObj,
             mapTypeId: google.maps.MapTypeId.ROADMAP
             });
 
+            //Plane on the map.
             var marker = new google.maps.Marker(
             {
-            position: myLatLng,
+            position: LatLongObj,
             map: map,
             title: 'Plane',
             icon: 'src/images/airplane.png'
             });
 
+            //Arrow which points in the direction of the wind.
             var lineSymbol = {
                 path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW,
-                rotation: forecastData.data.currently.windBearing,
+                rotation: forecastData.data.currently.windBearing, //Wind direction.
                 anchor: new google.maps.Point(0,7)
             };
 
@@ -70,6 +72,7 @@
                 });
             }
 
+            //Sets the weather metrics.
             function setData(weatherData){
                    SetupSkyIcon(weatherData.data.currently.icon);
                    $scope.currentTemp = weatherData.data.currently.apparentTemperature;
